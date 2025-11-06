06f71c2051e871e0c459e180ca5acbf968e176ca
Kento103 <kento103@outlook.com>
2025-11-06 23:07:14 +0900

    内容追加 #1
    くるくる回るロゴを高速化！
    ロゴを追加！
    ボタンも追加！

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import reactKawaiiLogo from '/React_sawalogo.png'
+import nodejsKawaiiLogo from '/Node_sawalogo.png'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const testClick = () => {
+    alert("ボタンをクリックしました！\nこんにちは！");
+  }
+  const counterShow = () => {
+    alert("現在" + count + "回ボタンをクリックしました。");
+  }
 
   return (
     <>
@@ -19,15 +27,25 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          {count} 回クリックしたよ
         </button>
+        <button onClick={testClick}>ポップアップ表示</button>
+        <button onClick={counterShow}>現在のクリック回数を表示する</button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          <code>src/App.jsx</code> を編集したり保存したりして色々いじってあそんでみてね。
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        ViteとReactのロゴをクリックすると、詳細を公式ページで確認できます。
       </p>
+      <div>
+        <a href="https://nodejs.org" target='_blank'>
+          <img src={nodejsKawaiiLogo} className="logo" alt='Node.jsさわらつきさん' />
+        </a>
+        <a href="https://react.dev" target='_blank'>
+          <img src={reactKawaiiLogo} className="logo" alt="Reactさわらつきさん" />
+        </a>
+      </div>
     </>
   )
 }
